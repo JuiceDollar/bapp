54e318d00731e25785bd866cae164c203cd22d41
Jonathan <Jonathanchw@gmail.com>
2025-12-23 14:35:24 -0300

    remove Back button and simplify AdjustExpiration component

--- pages/mint/[address]/manage/expiration.tsx
+++ pages/mint/[address]/manage/expiration.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
-import { useRouter } from "next/router";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { SectionTitle } from "@components/SectionTitle";
 import { AdjustExpiration } from "@components/PageMint/AdjustExpiration";
@@ -9,8 +8,6 @@ import { TOKEN_SYMBOL } from "@utils";
 
 export default function ManageExpiration() {
 	const { t } = useTranslation();
-	const router = useRouter();
-	const { address: addressQuery } = router.query;
 
 	return (
 		<>
@@ -22,7 +19,7 @@ export default function ManageExpiration() {
 			<div className="md:mt-8 flex justify-center">
 				<AppCard className="max-w-lg w-full p-6 flex flex-col gap-y-6">
 					<SectionTitle className="!mb-0 text-center !text-xl">{t("mint.adjust_your_borrowing_position")}</SectionTitle>
-					<AdjustExpiration onBack={() => router.push(`/mint/${addressQuery}/manage`)} />
+					<AdjustExpiration />
 				</AppCard>
 			</div>
 		</>
