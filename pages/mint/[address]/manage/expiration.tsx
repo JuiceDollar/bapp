0e308b6ffd7eb00a9313bfae482830c07c11b61a
Jonathan <Jonathanchw@gmail.com>
2025-12-24 16:01:53 -0300

    refactor: extract useExtensionTarget hook and add minimumCollateral filter

--- pages/mint/[address]/manage/expiration.tsx
+++ pages/mint/[address]/manage/expiration.tsx
@@ -1,13 +1,31 @@
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { SectionTitle } from "@components/SectionTitle";
 import { AdjustExpiration } from "@components/PageMint/AdjustExpiration";
 import AppCard from "@components/AppCard";
 import { TOKEN_SYMBOL } from "@utils";
+import { usePositionManageData } from "../../../../hooks/usePositionManageData";
 
 export default function ManageExpiration() {
 	const { t } = useTranslation();
+	const router = useRouter();
+	const { address: addressQuery } = router.query;
+
+	const { position, isLoading } = usePositionManageData(addressQuery);
+
+	if (isLoading || !position) {
+		return (
+			<div className="md:mt-8 flex justify-center">
+				<AppCard className="max-w-lg w-full p-6 flex flex-col gap-y-6">
+					<div className="flex justify-center items-center h-64">
+						<span className="text-text-muted2">Loading...</span>
+					</div>
+				</AppCard>
+			</div>
+		);
+	}
 
 	return (
 		<>
@@ -19,7 +37,7 @@ export default function ManageExpiration() {
 			<div className="md:mt-8 flex justify-center">
 				<AppCard className="max-w-lg w-full p-6 flex flex-col gap-y-6">
 					<SectionTitle className="!mb-0 text-center !text-xl">{t("mint.adjust_your_borrowing_position")}</SectionTitle>
-					<AdjustExpiration />
+					<AdjustExpiration position={position} />
 				</AppCard>
 			</div>
 		</>
