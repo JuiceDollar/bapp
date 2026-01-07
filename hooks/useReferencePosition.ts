2458928354c4d301785fb44d24de07941a6a48b4
Jonathan <Jonathanchw@gmail.com>
2026-01-07 15:54:27 -0300

    remove hardcoded Default position

--- hooks/useReferencePosition.ts
+++ hooks/useReferencePosition.ts
@@ -30,36 +30,21 @@ export const useReferencePosition = (
 		fetchDefaultPosition();
 	}, [defaultPosition, dispatch]);
 
-	const hardcodedDefaultPosition: PositionQuery = {
-		// TODO: remove this after testing and API fix is finished
-		position: "0xE2D4Ca089457ECfabF89F472568eac4e94b21d8C",
-		collateral: "0x8d0c9d1c17aE5e40ffF9bE350f57840E9E66Cd93",
-		collateralSymbol: "WCBTC",
-		collateralDecimals: 18,
-		price: "50000000000000000000000",
-		minimumCollateral: "2000000000000000",
-		availableForClones: "99997499204047936896691370",
-		expiration: 1797341717,
-		reserveContribution: 200000,
-		annualInterestPPM: 100000,
-		principal: 500000000000000000000, // notice that in the blockchain the default position has pricipal = 0; the function _isValidPriceReference need to be fixed in position.sol to prevent the revert.
-	} as unknown as PositionQuery;
-
 	return useMemo(() => {
 		if (!currentPosition) return { address: null, price: 0n };
 
 		if (
-			hardcodedDefaultPosition && // TODO: change this to defaultPosition this after testing and API fix is finished
-			hardcodedDefaultPosition.collateral.toLowerCase() === currentPosition.collateral.toLowerCase() &&
-			BigInt(hardcodedDefaultPosition.price) > currentPrice &&
-			hardcodedDefaultPosition.principal
+			defaultPosition &&
+			defaultPosition.collateral.toLowerCase() === currentPosition.collateral.toLowerCase() &&
+			BigInt(defaultPosition.price) > currentPrice &&
+			defaultPosition.principal
 		) {
 			return {
-				address: hardcodedDefaultPosition.position as Address,
-				price: BigInt(hardcodedDefaultPosition.price),
+				address: defaultPosition.position as Address,
+				price: BigInt(defaultPosition.price),
 			};
 		}
 
 		return { address: null, price: 0n };
-	}, [currentPosition, currentPrice, hardcodedDefaultPosition]);
+	}, [currentPosition, currentPrice, defaultPosition]);
 };
